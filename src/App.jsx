9dbe527cd6dfe0a37c5a388074a94fa3c61eee86
saeadyou <saeadyou@gmail.com>
2024-03-14 12:48:31 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="attribution">
+      Challenge by
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        Frontend Mentor
+      </a>
+      . Coded by <a href="#">Mahdi Yousefi</a>.
+    </div>
+  );
+}
+
+export default App;
