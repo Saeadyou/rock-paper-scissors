9d31f3947ec809eec4663a9dd4db4437ea18ce82
saeadyou <saeadyou@gmail.com>
2024-03-15 13:26:25 +0100

    create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 function App() {
   return (
-    <div>
-      Challenge by
-      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-        Frontend Mentor
-      </a>
-      . Coded by <a href="#">Mahdi Yousefi</a>.
-    </div>
+    <>
+      <header className="absolute left-1/2 top-10 flex w-[40rem] max-w-[80%] -translate-x-1/2 justify-between rounded-lg border p-4">
+        <div className="text-xs">
+          <h1>Rock</h1>
+          <h1>paper</h1>
+          <h1>scissors</h1>
+        </div>
+        <div className="flex flex-col items-center justify-center rounded-md bg-white px-8">
+          <span className="text-xs text-blue-500">Score</span>
+          <span className="text-4xl font-bold text-gray-600">12</span>
+        </div>
+      </header>
+      <main></main>
+      <button className="uppercase">Rules</button>
+    </>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: "Rubik, system-ui, Avenir, Helvetica, Arial, sans-serif",
+    },
     extend: {},
   },
   plugins: [],
