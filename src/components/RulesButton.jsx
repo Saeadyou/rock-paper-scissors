ef6458c3c5050f25b7b7bbb1f95a9d05db2f8ef9
saeadyou <saeadyou@gmail.com>
2024-03-30 15:04:05 +0100

    fix a bug - center rules button

--- src/components/RulesButton.jsx
+++ src/components/RulesButton.jsx
@@ -6,7 +6,7 @@ function RulesButton() {
   return (
     <button
       onClick={() => setShowModal(true)}
-      className="absolute bottom-12 right-1/2 w-fit -translate-x-1/2 border border-green-300 bg-transparent px-8 text-xs uppercase sm:bottom-6 sm:right-6 sm:translate-x-0"
+      className="absolute bottom-12 right-1/2 w-fit translate-x-1/2 border border-green-300 bg-transparent px-8 text-xs uppercase sm:bottom-6 sm:right-6 sm:translate-x-0"
     >
       Rules
     </button>
